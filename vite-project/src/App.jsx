38e445d952411f3482c385245f6dce5ee19d37bb
Soumya1432 <biswassoumya17@gmail.com>
2023-09-27 22:58:13 +0530

    created first step

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-700'>
+      Hello world</h1>
+
+      <h2 className='first-letter:bg-red-700'>dfkvb</h2>
+
+      <div className='bg-orange-300'>edfbjdfb</div>
+      <Home/>
+    </div>
+  )
+}
+
+export default App;
