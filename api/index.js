75f68414aed6d762140ac01a23d4615a2db25300
Soumya1432 <biswassoumya17@gmail.com>
2023-09-28 00:31:00 +0530

    database and usermodule

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function database(){
+await mongoose.connect(process.env.MONGO).then(()=>{
+     console.log("Connected to Mongo DB");
+
+}).catch((err)=>{
+    console.log(err);
+    
+})
+
+}
 
 const app=express();
-
+database();
 app.listen(3000, ()=>{
     console.log("Server running on port 3000 !!");
 
