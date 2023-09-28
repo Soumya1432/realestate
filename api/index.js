38270cb80e195ccfeb1b66588d85cefd89bd42ca
Soumya1432 <biswassoumya17@gmail.com>
2023-09-28 11:11:57 +0530

    create authentication of backend

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter  from './routes/user.route.js';
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-async function database(){
-await mongoose.connect(process.env.MONGO).then(()=>{
+//async function database(){
+mongoose.connect(process.env.MONGO).then(()=>{
      console.log("Connected to Mongo DB");
 
 }).catch((err)=>{
@@ -12,11 +14,37 @@ await mongoose.connect(process.env.MONGO).then(()=>{
     
 })
 
-}
+
 
 const app=express();
-database();
+app.use(express.json());
+
+//database();
 app.listen(3000, ()=>{
     console.log("Server running on port 3000 !!");
 
 });
+
+
+//api route
+
+// app.get('/test',(req,res)=>{
+//     res.json({
+//         message:"Hello world",
+//     });
+// });
+
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
+
+//user internal server error
+app.use((err,req,res,next) =>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal server error';
+    return res.status(statusCode).json({
+       success:false,
+       statusCode,
+       message,   
+    });
+})
